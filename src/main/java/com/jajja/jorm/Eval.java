43d70d5079e62e79919c1d6595623a9ebdd778d4
Martin Korinth <martin@korinth.se>
2014-06-02 11:58:20 +0200

    Feature draft for eval keys

---
+++ src/main/java/com/jajja/jorm/Eval.java
@@ -0,0 +1,21 @@
+package com.jajja.jorm;
+
+public class Eval extends Composite {
+
+    public Eval(String ... columns) {
+        if (columns.length == 0) {
+            throw new IllegalArgumentException("At least 1 column is required");
+        }
+        this.symbols = new Symbol[columns.length];
+        for (int i = 0; i < columns.length; i++) {
+            this.symbols[i] = Symbol.get(columns[i], true);
+        }
+    }
+
+    public Eval(Symbol ... symbols) {
+        if (symbols.length == 0) {
+            throw new IllegalArgumentException("At least 1 symbol is required");
+        }
+        this.symbols = symbols;
+    }
+}
