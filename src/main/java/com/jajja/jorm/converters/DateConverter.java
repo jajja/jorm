a2743cfeca3628a80c40940a97f1840447c8b258
Andreas Allerdahl <dnk@jajja.com>
2017-03-03 09:37:32 +0100

    zzz

--- src/main/java/com/jajja/jorm/converters/DateConverter.java
+++ src/main/java/com/jajja/jorm/converters/DateConverter.java
@@ -1,10 +1,13 @@
 package com.jajja.jorm.converters;
 
 
+
 public abstract class DateConverter<T> implements TypeConverter<T> {
 //    protected static final BigDecimal floatMaxValue = new BigDecimal(Float.MAX_VALUE);
 //    protected static final BigDecimal doubleMaxValue = new BigDecimal(Double.MAX_VALUE);
 //    private static final Map<Class<? extends Number>, DateConverter<?>> map = new HashMap<Class<? extends Number>, DateConverter<?>>();
+//    private static boolean haveJodaTime = false;
+//    private static boolean haveJava8Time = false;
 //
 //    static {
 //        map.put(Date.class, new ByteConverter());
@@ -13,11 +16,13 @@ public abstract class DateConverter<T> implements TypeConverter<T> {
 //        try {
 //            map.put(org.joda.time.DateTime.class, new LongConverter());
 //            map.put(org.joda.time.LocalDate.class, new BigIntegerConverter());
+//            haveJodaTime = true;
 //        } catch (ClassNotFoundException e) {
 //        }
 //        try {
 //            map.put(java.time.OffsetDateTime.class, new BigDecimalConverter());
 //            map.put(java.time.LocalDate.class, new BigDecimalConverter());
+//            haveJava8Time = true;
 //        } catch (ClassNotFoundException e) {
 //        }
 //    }
@@ -56,10 +61,27 @@ public abstract class DateConverter<T> implements TypeConverter<T> {
 //        }
 //        throw new IllegalArgumentException("Unknown date class " + o.getClass());
 //    }
+////
+////    @Override
+////    public boolean supports(Object nonNullValue) {
+////        return map.containsKey(nonNullValue.getClass());
+////    }
 //
-//    @Override
-//    public boolean supports(Object nonNullValue) {
-//        return map.containsKey(nonNullValue.getClass());
+//    public static void main(String[] args) {
+//        ZonedDateTime a = ZonedDateTime.now();
+//        OffsetDateTime b = OffsetDateTime.now();
+//        LocalDate c = LocalDate.now();
+//        Date d = new Date(System.currentTimeMillis());
+//        Timestamp e = new Timestamp(System.currentTimeMillis());
+//        org.joda.time.DateTime f = new org.joda.time.DateTime(DateTimeZone.UTC);
+//        org.joda.time.LocalDate g = new org.joda.time.LocalDate();
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+//        System.out.println(g);
 //    }
-
 }
